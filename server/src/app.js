d8a5c781056bb95a1302931df0f725dad5d77dae
Ivan Shastin <ivan.shastin@gmail.com>
2017-09-26 21:53:59 +0200

    fixes wrong import in require('cors')

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('morgan');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
